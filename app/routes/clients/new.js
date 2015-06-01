877e24562176b7dd4475526c0a6c92182b75d6ef
Will Prahl <fotingkempchee@gmail.com>
2015-06-01 01:28:03 -0400

    first commit

---
+++ app/routes/clients/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+export default Ember.Route.extend({
+  actions:{
+    createClient:function(){
+      var route=this;
+      jQuery.ajax({
+        method:"POST",
+        url:"http://localhost:3000/clients",
+        data:JSON.stringify({id:0,name:this.get("controller.name")}),
+        success:function(client){
+          route.store.push("client",client);
+          route.transitionTo("clients");
+        },error:function(){
+
+        }
+      })
+    }
+  }
+});
